dc5189922ab91a23ac2284c1a5981b78db28aa93
farukbaykara <farukbaykarax@gmail.com>
2023-05-04 18:08:03 +0300

    add ntrip client
    tested(success)

--- include/ntrip_client_ros/ntrip_client_ros.hpp
+++ include/ntrip_client_ros/ntrip_client_ros.hpp
@@ -6,8 +6,54 @@
 #define NTRIP_CLIENT_ROS_NTRIP_CLIENT_ROS_H
 
 
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <cstring>
 
 
+#include <ntrip/ntrip_client.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include "nmea_msgs/msg/gpgga.h"
+#include "mavros_msgs/msg/rtcm.hpp"
+
+#include "AsyncSerial.h"
+
+class NtripClientRos: public rclcpp::Node
+{
+public:
+
+  NtripClientRos();
+
+
+private:
+
+  bool NtripClientStart();
+
+  void ReadParameters();
+
+  //Parameters
+  std::string m_serial_port_;
+  long m_serial_baud_rate_;
+  std::string m_ntrip_ip_;
+  std::string m_ntrip_password_;
+  std::string m_ntrip_username_;
+  std::string m_ntrip_mountpoint_;
+  long m_ntrip_port_;
+  bool m_publish_rtcm_;
+  bool m_debug_;
+  double m_ntrip_location_lat;
+  double m_ntrip_location_lon;
+
+  libntrip::NtripClient m_ntripClient_;
+
+  uint8_t m_ntripStatus_;
+
+
+};
 
 
 
--- src/ntrip_client_ros.cpp
+++ src/ntrip_client_ros.cpp
@@ -2,4 +2,117 @@
 // Created by farukbaykara on 04.05.2023.
 //
 
-#include "../include/ntrip_client_ros/ntrip_client_ros.h"
+#include "../include/ntrip_client_ros/ntrip_client_ros.hpp"
+
+using namespace std;
+using namespace chrono_literals;
+using rcl_interfaces::msg::ParameterDescriptor;
+using rclcpp::ParameterValue;
+
+NtripClientRos::NtripClientRos():Node("ntrip_client_ros"),
+  m_serial_port_{this->declare_parameter(
+                       "serial_port",
+                       ParameterValue{"/dev/ttyUSB0"},
+                       ParameterDescriptor{})
+                   .get<std::string>()},
+  m_serial_baud_rate_{this->declare_parameter(
+                       "serial_port_baud_rate",
+                       ParameterValue{460800},
+                       ParameterDescriptor{})
+                   .get<long>()},
+  m_ntrip_ip_{this->declare_parameter(
+                       "ntrip_ip",
+                       ParameterValue{"212.156.70.42"},
+                       ParameterDescriptor{})
+                   .get<std::string>()},
+  m_ntrip_username_{this->declare_parameter(
+                    "ntrip_user_name",
+                    ParameterValue{"K0734151301"},
+                    ParameterDescriptor{})
+                .get<std::string>()},
+  m_ntrip_password_{this->declare_parameter(
+                    "ntrip_password",
+                    ParameterValue{"GzMSQg"},
+                    ParameterDescriptor{})
+                .get<std::string>()},
+  m_ntrip_mountpoint_{this->declare_parameter(
+                    "ntrip_mount_point",
+                    ParameterValue{"VRSRTCM31"},
+                    ParameterDescriptor{})
+                .get<std::string>()},
+  m_ntrip_port_{this->declare_parameter(
+                    "ntrip_port",
+                    ParameterValue{2101},
+                    ParameterDescriptor{})
+                .get<long>()},
+  m_ntrip_location_lat{this->declare_parameter(
+                      "ntrip_location_lat",
+                      ParameterValue{41.018893949},
+                      ParameterDescriptor{})
+                  .get<double>()},
+  m_ntrip_location_lon{this->declare_parameter(
+                      "ntrip_location_lon",
+                      ParameterValue{28.890924848},
+                      ParameterDescriptor{})
+                  .get<double>()},
+
+  m_publish_rtcm_{this->declare_parameter(
+                    "publish_ros_rtcm_active",
+                    ParameterValue{true},
+                    ParameterDescriptor{})
+                .get<bool>()},
+  m_debug_{this->declare_parameter(
+                    "debug",
+                    ParameterValue{true},
+                    ParameterDescriptor{})
+                .get<bool>()}
+
+{
+  RCLCPP_INFO(this->get_logger(),"NTrip Client ROS created");
+  ReadParameters();
+
+  if(NtripClientStart()){
+    RCLCPP_INFO(this->get_logger(), "\033[1;32m NTRIP client connected \033[0m");
+  }
+  else{
+    RCLCPP_INFO(this->get_logger(), "\033[1;31m NTRIP client cannot connected \033[0m");
+
+  }
+
+
+}
+
+bool NtripClientRos::NtripClientStart()
+{
+  m_ntripClient_.Init(m_ntrip_ip_,m_ntrip_port_,m_ntrip_username_,m_ntrip_password_,m_ntrip_mountpoint_);
+
+  m_ntripClient_.OnReceived([this](const char *buffer, int size)
+                         {
+                           if(m_debug_ == true){
+                             RCLCPP_INFO(this->get_logger(), "NTRIP Data size: %d",size);
+                             RCLCPP_INFO(this->get_logger(), "NTRIP Status: %d",m_ntripClient_.service_is_running());
+                           }
+                         });
+
+
+  m_ntripClient_.set_location(m_ntrip_location_lat,m_ntrip_location_lon);
+  m_ntripClient_.set_report_interval(0.001);
+  return m_ntripClient_.Run();
+
+}
+
+void NtripClientRos::ReadParameters()
+{
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m Parameters\033[0m");
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m Serial Port Name:\033[0m %s",m_serial_port_.c_str());
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP Server ID:\033[0m %s",m_ntrip_ip_.c_str());
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP UserName:\033[0m %s",m_ntrip_username_.c_str());
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP Password:\033[0m %s",m_ntrip_password_.c_str());
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP MountPoint:\033[0m %s",m_ntrip_mountpoint_.c_str());
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP Port:\033[0m %d",m_ntrip_port_);
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP Location Lat:\033[0m %f",m_ntrip_location_lat);
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m NTRIP Location Lon:\033[0m %f",m_ntrip_location_lon);
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m Publish RTCM Msg via ROS:\033[0m %B",m_publish_rtcm_);
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m Debug:\033[0m %B",m_debug_);
+  RCLCPP_INFO(this->get_logger(), "\033[1;34m -----------------------------------------------------");
+}
